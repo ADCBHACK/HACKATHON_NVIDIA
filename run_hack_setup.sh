cac953b23423bdb3fb07d3a5e548af195ebf4935
ADCBHACK <t.grichi@gmail.com>
2024-10-03 11:45:46 +0400

    Update run_hack_setup.sh

--- run_hack_setup.sh
+++ run_hack_setup.sh
@@ -15,7 +15,7 @@ docker run \
     --gpus all \
     -p 8888:8888 \
     --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
-    -e EXTRA_PIP_PACKAGES=["langchain_nvidia_ai_endpoints", "googledriver"] \
+    -e EXTRA_PIP_PACKAGES="langchain_nvidia_ai_endpoints" \
     -e NB_PREFIX="/notebook" \
     -v $PWD/hack:/home/rapids/notebooks/hack \
     rapidsai/notebooks:24.08-cuda11.8-py3.9
